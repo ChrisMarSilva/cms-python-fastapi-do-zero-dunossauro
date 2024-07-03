4dbf9fc4253864b87289ac22ffb43812b61fb303
ChrisMarSilva <chris.mar.silva@gmail.com>
2024-07-03 20:09:54 -0300

    Aula 02 - Fundamentos do desenvolvimento web

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
+
+# https://fastapidozero.dunossauro.com/
+# pip freeze > requirements.txt
+# fastapi dev main.py
