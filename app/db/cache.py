8cc2033a8ebbb11e783780edbc6459a20389b35f
ChrisMarSilva <chris.mar.silva@gmail.com>
2024-07-11 18:02:54 -0300

    Add cache with Redis

--- app/db/cache.py
+++ app/db/cache.py
@@ -6,19 +6,17 @@ import redis
 # from redis.asyncio import Redis
 # import redis.asyncio as aioredis
 
+from app.utils.settings import Settings
+
 tracer = trace.get_tracer(__name__)
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_PASSWORD = '123'
-REDIS_DB = 0
-REDIS_MAX_CONNECTIONS = 100
+settings = Settings()
 
 
 async def get_cache() -> AsyncIterator[redis.Redis]:
     with tracer.start_as_current_span('get_cache'):
         # cache = await aioredis.create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}", password=REDIS_PASSWORD, encoding="utf-8", decode_responses=True)
         # cache = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", password=REDIS_PASSWORD, encoding="utf-8", decode_responses=True)
-        session = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_DB, encoding="utf-8", decode_responses=True)
+        session = redis.ConnectionPool(host=settings.REDIS_HOST, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD, db=settings.REDIS_DB, encoding="utf-8", decode_responses=True)
         cache = redis.Redis(connection_pool=session)
         yield cache
         cache.close()
--- app/utils/settings.py
+++ app/utils/settings.py
@@ -4,8 +4,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
 
-    DATABASE_URL: str
-    DATABASE_ECHO: bool = False
+    # SECURY
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    # BD
+    DATABASE_URL: str
+    DATABASE_ECHO: bool = False
+
+    # CACHE
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+    REDIS_DB: int
+    REDIS_MAX_CONNECTIONS: int
